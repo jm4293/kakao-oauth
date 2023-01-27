b0b5e013396b7cd4b30e989dcb571b216a25e3e2
jm4293 <107846495+jm4293@users.noreply.github.com>
2023-01-27 23:35:15 +0900

    feat: 로그아웃

---
+++ src/Components/Logout.jsx
@@ -0,0 +1,40 @@
+import React, {useEffect} from "react";
+import styled from "styled-components";
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
+
+function Logout() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        async function logout() {
+            await axios.get('http://localhost:8000/logout')
+                .then((result) => {
+                    sessionStorage.removeItem(result.data)
+                    alert("로그아웃 성공")
+                    navigate('/')
+                })
+                .catch((error) => {
+                    console.log(error)
+                    alert("로그아웃 실패")
+                })
+        }
+        logout();
+    })
+
+    return (
+        <Frame>
+            <h1>잠시만 기다려 주세요! 로그아웃 중입니다!</h1>
+        </Frame>
+    )
+}
+
+const Frame = styled.div`
+  width: 100%;
+  height: 86%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default Logout;
