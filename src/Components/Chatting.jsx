06772ab711a7ae5b68e1daff2cfefe26f4446f42
jm4293 <107846495+jm4293@users.noreply.github.com>
2023-01-28 22:34:41 +0900

    feat: 채팅목록에 프로필 사진 추가

--- src/Components/Chatting.jsx
+++ src/Components/Chatting.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import styled from "styled-components";
 import axios from "axios";
+import {useSelector} from "react-redux";
 
 function Chatting() {
+    let img = useSelector((state) => {
+        return state.profile.img;
+    })
+
     const click = async () => {
         await axios.get('http://localhost:8000/chatting')
     }
 
     return(
         <Frame>
-            <h1>Chatting</h1>
+            <h1 style={{borderBottom: "1px solid rgb(222, 222, 222)"}}>Chatting</h1>
+            <Photo src={img}/>
             <button onClick={() => click()}>나한테 메세지 보내기</button>
         </Frame>
     )
@@ -18,6 +24,13 @@ function Chatting() {
 const Frame = styled.div`
   width: 100%;
   height: 86%;
+`;
+
+const Photo = styled.img`
+  width: 10%;
+  height: 10%;
+  margin: 0 20px;
+  border-radius: 30px;
 `;
 
 export default Chatting
